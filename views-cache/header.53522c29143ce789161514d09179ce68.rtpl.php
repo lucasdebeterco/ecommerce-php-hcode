a3375c8821fb1589dd2f8ca4395872049f04afad
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-09-08 00:11:53 -0300

    Configurando carregamento do template pela classe Page.php

---
+++ views-cache/header.53522c29143ce789161514d09179ce68.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hcode Store</title>
+</head>
+<body>
