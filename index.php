1ac446a98eb9d6837ad1c92c4573399e2ef19446
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-09-16 00:30:49 -0300

    Fazendo tratamento de login no admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -25,6 +26,12 @@ $app->get('/admin/login', function() {
         "footer" => false
     ]);
     $page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+    User::login($_POST["login"], $_POST["password"]);
+
+    header("Location: /admin");
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hcode;
+
+class Model {
+    private $values = [];
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+    }
+}
